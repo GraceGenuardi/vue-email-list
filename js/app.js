32e9189bca34ba9fded61087f1485b26660a31df
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-27 14:51:46 +0100

    FEAT incluso axios

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@
 const generateBtn = document.getElementById('generateBtn');
 const emailList = document.getElementById('emailList');
 
+//2-AXIOS + lista email
+
+generateBtn.addEventListener('click', () => {
+	axios.all([
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail'),
+		axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        
+	])
+	
+});
+
